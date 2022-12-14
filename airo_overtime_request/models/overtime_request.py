d8ebd58181b76ddff19fe348376a7cb322c1d07f
jedie boy calut <jedieboycalut@jedies-MacBook-Pro-2.local>
2022-12-14 10:11:42 +0800

    bug fixes completed

--- airo_overtime_request/models/overtime_request.py
+++ airo_overtime_request/models/overtime_request.py
@@ -30,7 +30,6 @@ class airo_overtime_request(models.Model):
         default='rush',
         readonly=False, required=True)
 
-
     @api.model
     def _get_default_name(self):
         user = self.env['res.users'].has_group('hr.group_hr_user')
